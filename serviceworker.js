6c765f7d69c0c5734ffdcda3748c2b4bf72e511a
Tim Vandehey <tim.vandehey@gmail.com>
2024-05-06 14:49:08 -0500

    first GIRISI try

---
+++ serviceworker.js
@@ -0,0 +1,24 @@
+var staticCacheName = "pwa";
+
+self.addEventListener("install", function (e) {
+// @ts-ignore
+e.waitUntil(
+	caches.open(staticCacheName).then(function (cache) {
+	return cache.addAll(["/"]);
+	})
+);
+});
+
+self.addEventListener("fetch", function (event) {
+// @ts-ignore
+console.log(event.request.url);
+
+// @ts-ignore
+event.respondWith(
+	// @ts-ignore
+	caches.match(event.request).then(function (response) {
+	// @ts-ignore
+	return response || fetch(event.request);
+	})
+);
+});
